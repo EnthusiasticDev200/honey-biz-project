11931a00f3e2b8817569feb35bbcd1bab2ea9622
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-09-06 16:53:36 +0100

    Modified generateOrderitems logic and creted myOrderItems and viewOrderItems

--- backend/controllers/authControllers/orderItemControllers.js
+++ backend/controllers/authControllers/orderItemControllers.js
@@ -1,31 +1,39 @@
+import  dotenv  from 'dotenv';
 import db from '../../config/database.js';
-import { identifyCustomer } from './orderControllers.js';
+import positiveIntParam from '../../../utils/paramInput.js'
 
-
+dotenv.config()
 
 const generateOrderItems = async (req, res)=>{
     try{
-        const getCustomerId = identifyCustomer(req)
-        const customerId = getCustomerId[0]
+        const customerId = req.customerId
 
-        // remove amount from req.body and from db too. autocreate it
-        // unit price should be from products 
-        const {productName, quantity, unitPrice, amount} = req.body
+        const {productName, quantity, unitPrice} = req.body
 
         const queryProduct = await db.query(`
-            SELECT * FROM products WHERE product_name =$1`
+            SELECT * FROM products WHERE product_name =$1`,
             [productName])
         if(queryProduct.rows.length === 0){
             return res.status(400).json({message: 'Invalid product'})
         }
-        //extract productId, and stockQuantity. unitPrice later
+       
+        //extract productId, stockQuantity. and unitPrice 
         const productId = queryProduct.rows[0].product_id
-        // const unitPrice = queryProduct.rows[0].unit_price
+       
         const stockQuantity = queryProduct.rows[0].stock_quantity
-
+        
+        const productPrice = queryProduct.rows[0].price
+        console.log("productNames in productsDB: ", productPrice)
+        
         if(quantity > stockQuantity){
             return res.status(400).json(
-                {message: `${productName} is cuurently out stock`})
+                {message: `${productName} is cuurently out of stock`})
+        }
+        
+        if(unitPrice < productPrice || unitPrice > productPrice ){
+            return res.status(400).json({
+                message : `${productName} is sold for ${productPrice}`
+            })
         }
         //extract orderId
         const myOrder = await db.query(`
@@ -42,8 +50,11 @@ const generateOrderItems = async (req, res)=>{
         const existingOrderItems = await db.query(`
             SELECT * 
                 FROM order_items
-            WHERE product_id, order_id, quantity, unit_price, amount
-            `, [productId, orderId, quantity, unitPrice, amount])
+                WHERE product_id = $1
+                    AND order_id = $2 
+                    AND quantity = $3 
+                    AND unit_price = $4
+            `, [productId, orderId, quantity, unitPrice])
         if(existingOrderItems.rows.length === 1){
             return res.status(400).json({
                 message: 'Order items already generated'
@@ -51,9 +62,10 @@ const generateOrderItems = async (req, res)=>{
         }
         const createOrderItem = await db.query(`
             INSERT INTO order_items 
-                (product_id, order_id, quantity, unit_price, amount)
-            VALUES($1, $2, $3, $4, $5)`
-            ,[productId, orderId, quantity, unitPrice, amount])
+                (order_id, product_id, quantity, unit_price)
+            VALUES($1, $2, $3, $4)
+            RETURNING order_item_id;`
+            ,[orderId, productId, quantity, unitPrice])
         // extract createdOrderItems data
         const orderItems = createOrderItem.rows[0]
         
@@ -64,7 +76,8 @@ const generateOrderItems = async (req, res)=>{
             orderId : orderItems.order_id,
             quantity : orderItems.quantity
         }
-        // add socketIO func here to update db
+        console.log("orderItemsData: ", OrderItemsData)
+    
          
         return res.status(201).json({
             message:'Order items created successfully'})
@@ -76,10 +89,99 @@ const generateOrderItems = async (req, res)=>{
         })
     }
 }
+const myOrderItems = async (req, res)=>{
+    const customerId = req.customerId
+    const customerUsername = req.customerUsername
+    const isCustomer = await db.query(`
+        SELECT email FROM customers WHERE username = $1`, [customerUsername])
+    if(isCustomer.rows.length === 0) return res.status(401).json({
+        message : "You're not authorized"
+    })
+    try{    
+        const placedOrder = await db.query(`
+            SELECT order_id FROM orders WHERE customer_id = $1`, [customerId])
+        if(placedOrder.rows.length === 0){
+            return res.status(404).json({message: "No record. Please kindly please order"})
+        }
+        const order = placedOrder.rows[0].order_id
+        const myItems = await db.query(`
+            SELECT
+                c.username AS username, order_id, 
+                pr.product_name AS products, quantity, unit_price, amount
+            FROM order_items
+            JOIN products pr USING (product_id)
+			JOIN orders o USING (order_id)
+            JOIN customers c USING (customer_id)
+			WHERE order_id = $1
+			ORDER BY order_id DESC
+        `, [order])
+        return res.status(200).json(myItems.rows)
+    }catch(err){
+        console.log("Error fetching order items for customer", err)
+        return res.status(500).json({
+            message: "Failed retrieving customer's order items",
+            error : err.stack
+        })
+    }
+
+}
+
+const viewOrderItems = async (req, res) =>{
+    const { order_id } = req.params
+    const role = req.role
+    if(role !== process.env.SUPER_USER) return res.status(401).json({
+        message : "Only for super users"
+    })
+    try{
+        if(order_id){
+            try{
+                let orderId
+                orderId = positiveIntParam(order_id)
+                const getOrderItem = await db.query(`
+                    SELECT
+                        order_item_id, c.username AS username, order_id, 
+                        pr.product_name AS products, quantity, unit_price, amount
+                    FROM order_items
+                    JOIN products pr USING (product_id)
+                    JOIN orders o USING (order_id)
+                    JOIN customers c USING (customer_id)
+                    WHERE order_id = $1
+                    ORDER BY order_id DESC
+                    `, [orderId])
+                if(getOrderItem.rows.length === 0){
+                    return res.status(200).json({
+                        message: "No order items record for this customer"})
+                }return res.status(200).json(getOrderItem.rows)
+            }catch(err){
+                console.log("Expected integer param: ",err)
+                return res.status(400).json({message: err.message})
+            }
+        }else{
+            const getAllOrderItems = await db.query(`
+                SELECT
+                    order_item_id, c.username AS username, order_id, 
+                    pr.product_name AS products, quantity, unit_price, amount
+                FROM order_items
+                JOIN products pr USING (product_id)
+                JOIN orders o USING (order_id)
+                JOIN customers c USING (customer_id)
+                ORDER BY order_id DESC
+                `)
+            return res.status(200).json(getAllOrderItems.rows)
+        }
+    }catch(err){
+        console.log("Error in fetching ordered items: ", err)
+        return res.status(500).json({
+            message : "Error occurred getting ordered items",
+            error : err.stack
+        })
+    }
+
+}
 
 
 
-export {generateOrderItems}
+export {generateOrderItems, myOrderItems, viewOrderItems}
 
 
 
