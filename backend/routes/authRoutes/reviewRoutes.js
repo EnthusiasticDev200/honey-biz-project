9ad09e26773f0cc06c129ca1f13699f2d1d9f1c1
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-09-09 14:49:10 +0100

    Created routes for reviews

---
+++ backend/routes/authRoutes/reviewRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {customerOnly, validateJWTAcessToken } from '../../../middlewares/auth.js'
+import createReview from "../../controllers/authControllers/reviewControllers.js";
+
+const router = express.Router()
+
+
+router.post('/create', validateJWTAcessToken, customerOnly, createReview )
+
+
+
+
+
+
+
+
+
+
+
+export {router as reviewRouter}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
