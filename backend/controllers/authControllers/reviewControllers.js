9541024b7414eaaf9fe389c36f17d35d7d7fbd8f
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-09-09 14:47:46 +0100

    Created reviews logic

---
+++ backend/controllers/authControllers/reviewControllers.js
@@ -0,0 +1,69 @@
+import db from "../../config/database.js"
+
+
+
+const createReview = async (req,res)=>{
+    const customerId = req.customerId
+    const { email, feedback, rating} = req.body
+    try{
+        const idCustomer = await db.query(`
+            SELECT customer_id, first_name, last_name, email  
+            FROM customers
+            WHERE email = $1
+            `, [email])
+        if(idCustomer.rows.length === 0){
+            return res.status(404).json({
+                message : 'Not a customer'
+            })
+        }
+        const checkReview = await db.query(`
+            SELECT * FROM reviews WHERE customer_id = $1`,
+            [customerId])
+        if(checkReview.rows.length > 0) return res.status(409).json({
+            message : "Review already created"
+        })
+        await db.query(`
+            INSERT INTO reviews 
+            VALUES (feedback, ratng)`, [feedback, rating])
+        return res.status(201).json({message: "Review successfully created"})
+    }catch(err){
+        console.log("Error occurred creating review: ", err)
+        return res.status(500).json({
+            message: "Creating reviews failed",
+            error : err.stack
+        })
+    }
+}
+
+
+export default createReview
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
