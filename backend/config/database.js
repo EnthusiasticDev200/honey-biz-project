a79c44819f83883493dd664a171776ea353b415c
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-09-22 23:51:45 +0100

    Restructured database config for Railway deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,12 +8,9 @@ const {Pool} = pg
 
 const db = new Pool(
   {
-    'user':process.env.DB_USERNAME,
-    'password': process.env.DB_PASSWORD,
-    'host': process.env.DB_HOST,
-    'port': process.env.DB_PORT,
-    'database':process.env.DB_NAME,
-  }
+    connectionString : process.env.DATABASE_URL,
+    ssl : {rejectUnauthorized : false} // for Railway
+  },
  );
 
 // measure db speed
@@ -22,9 +19,9 @@ db.query = async function (...args){
   const dbStart = performance.now()
   const result = await originalQuery.apply(this, args)
   const duration = performance.now() - dbStart
-  if(duration > 100){
-    console.warn(` Slow query (${duration.toFixed(2)} ms): ${args[0]}`);
-  }  console.log(`Query took ${duration} ms -> ${args[0]}`);
+  duration > 100 
+  ? console.warn(` Slow query (${duration.toFixed(2)} ms): ${args[0]}`)
+  : console.log(`Query took ${duration} ms -> ${args[0]}`);
   return result
 }
 export default db
