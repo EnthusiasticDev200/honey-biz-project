97f74d51be442dddc7cb190ee614d14792f5e81e
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-09-23 01:04:28 +0100

    Added list of sites that can access backend

---
+++ backend/cors.js
@@ -0,0 +1,10 @@
+
+const allowedOrigins = [
+  "http://localhost:3030",
+  "http://localhost:5173",
+  "https://khleancutz-salon.onrender.com",  // replace with Railway
+];
+
+
+
+export default allowedOrigins
